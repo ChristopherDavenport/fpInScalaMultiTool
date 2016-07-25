945d59ad6ef30836160d4f01cbc3cc8d39842be6
Christopher Davenport <ChristopherDavenport@outlook.com>
2016-07-25 08:11:56 -0400

    Starting Chapter 5

---
+++ src/main/scala/fpinscalamulti/Stream.scala
@@ -0,0 +1,68 @@
+package fpinscalamulti
+
+/**
+  * Created by davenpcm on 7/24/16.
+  */
+trait Stream[+A] {
+  /**
+    * Example HeadOption which returns the Option of the Head if it exists or None if the Stream is Empty
+    * @return An Option of Type A
+    */
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Con(h, t) => Some(h())
+  }
+
+  /**
+    * Exercise 5.1
+    * This function strictly evaluates the entire Stream
+    * @return A list of Type A
+    */
+  def toList: List[A] = this match {
+    case Con(h, t) =>
+      val head = h()
+      val tail = t()
+      head :: tail.toList
+    case Empty =>
+      List[A]()
+  }
+
+}
+
+case object Empty extends Stream[Nothing]
+case class Con[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+
+  /**
+    * Smart Constructor for creating a nonemepty stream.
+    * @param hd The head of the non empty stream
+    * @param tl The tail of the nonempty stream
+    * @tparam A The type of the stream
+    * @return A nonempty stream.
+    */
+  def con[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Con(() => head, () => tail)
+  }
+
+  /**
+    * A smart constructor for creatign an empty stream of a particular type
+    * We cache the head and tail as lazy values to avoid repeated evaluation
+    *
+    * @tparam A The type of the stream
+    * @return An Empty Stream
+    */
+  def empty[A]: Stream[A] = Empty
+
+  /**
+    * A convenient variable-argument method for constructing a Stream from multiple elements.
+    * @param as Variadic arguments to construct a stream
+    * @tparam A The type of the stream
+    * @return A Stream of type A
+    */
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else con(as.head, apply(as.tail: _*))
+
+}
---
+++ src/test/scala/fpinscalamulti/StreamTests.scala
@@ -0,0 +1,10 @@
+package fpinscalamulti
+
+import org.scalatest.{FlatSpec, Matchers}
+/**
+  * Created by davenpcm on 7/24/16.
+  */
+class StreamTests extends FlatSpec with Matchers {
+
+
+}

9cce4687213bb7469ec77640701fd7f6c325a8b0
Christopher Davenport <ChristopherDavenport@outlook.com>
2016-08-19 18:02:24 -0400

    Worked More On Chapter 5

--- src/main/scala/fpinscalamulti/Vanilla/laziness/Stream.scala
+++ src/main/scala/fpinscalamulti/Vanilla/laziness/Stream.scala
@@ -4,6 +4,9 @@ package fpinscalamulti.Vanilla.laziness
   * Created by davenpcm on 7/24/16.
   */
 trait Stream[+A] {
+
+  import Stream._
+
   /**
     * Example HeadOption which returns the Option of the Head if it exists or None if the Stream is Empty
     * @return An Option of Type A
@@ -25,6 +28,116 @@ trait Stream[+A] {
       head :: tail.toList
     case Empty =>
       List[A]()
+  }
+
+  /**
+    * From Example -> Stack Safe
+    * def toList: List[A] = {
+    *   AT annotation.tailrec
+    *   def go(s: Stream[A], acc: List[A] = s match {
+    *     case Conse(h,t) => go(t(), h():: acc)
+    *     case _ => acc
+    *   }
+    *   go(this, List()).reverse
+    * }
+    *
+    */
+
+  /**
+    * Exercise 5.2
+    * This works on the Stream to take a certain number of elements. It uses the smart constructor constructor for
+    * memoization, and then the tail continues through this process. Important to realize that the take method as a
+    * result has already evaluated all values in the stream that were taken
+    * @param i The number of elements from the stream to take
+    * @return A new stream with a number of elements equal to or less than depending on the original Stream with the
+    *         elements memoized.
+    */
+  def take(i: Int): Stream[A] = this match {
+    case Cons(h, t) if i > 1  => cons(h(), t().take(i - 1))
+    case Cons(h, _) if i == 1 =>  cons(h(), empty)
+    case _ => empty
+  }
+
+  /**
+    * Exercise 5.2
+    * This works by ignoring the head argument and continuing to evaluate the tail to create a new list, this has
+    * absolutely no evaluation as it merely gets the rest of the stream and continues on.  In the case i reaches
+    * zero it  returns the current stream, still unevaluated.
+    * @param i The number of values to drop
+    * @return A stream with that many fewer element.
+    */
+  @annotation.tailrec
+  final def drop(i: Int): Stream[A] = this match {
+    case Cons(_, t) if i > 0 => t().drop(i -1)
+    case _ => this
+  }
+
+  /**
+    * Take while takes a conditional an applies it to the head of the list, if it is empty, hence the end, we return
+    * whatever exists, if it meets the criteria we built the stream and continue evaluation on the tail. If it
+    * does not satisfy the value we apply empty in the final place replacing any of the following values in the stream
+    * without evaluation.
+    *
+    * Not stack safe..
+    *
+    * @param p The predicate to filter on
+    * @return A stream consisting of elements from the front of the stream until a value fails to meet the predicate.
+    */
+  def takeWhile1(p: A => Boolean): Stream[A] = this match {
+    case Empty => this
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile1(p))
+    case Cons(_, _) => empty
+  }
+
+  /**
+    * Example
+    *
+    * Since || is non strict in its second argument this only continues until if finds an element and then
+    * the computation stops.
+    *
+    * @param p The predicate to look for
+    * @return True if a value exists that meets the predicate, false if not.
+    */
+  def exists(p: A=> Boolean): Boolean = foldRight(false)((a,b) => p(a) || b)
+
+  /**
+    *
+    * @param z The value that is placed at the end of evaluation
+    * @param f The function that takes the aggregrate from the right and give a B
+    * @tparam B The returned value of the aggregation function
+    * @return A type B based on what the aggregat function implemented
+    */
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  /**
+    * If all values are true then it is true, if it is not then it is false and short circuits immediately
+    * @param p The predicate to search against
+    * @return A boolean representing whether all values of the stream match the predicate.
+    */
+  def forAll(p: A=> Boolean): Boolean = foldRight(true)((a,b) => p(a) && b)
+
+  /**
+    * Exercise 5.5
+    * @param p
+    * @return
+    */
+  def takeWhile(p: A => Boolean): Stream[A] = foldRight(Stream(): Stream[A]){
+    (a, b) => if (p(a)) cons(a, b.takeWhile(p)) else empty
+  }
+
+  /**
+    * Exercise 5.6
+    * Looks at the first element, if its Empty stops Evaluation, else returns A as a Some
+    * @return An Option depending on the first elements existence
+    */
+  def headOptionFoldR: Option[A] = foldRight(None: Option[A])((a, _) => if (isEmpty) None else Some(a))
+
+  def isEmpty: Boolean = this match {
+    case Empty => true
+    case _ => false
   }
 
 }
--- src/test/scala/fpinscalamulti/Vanilla/laziness/StreamTests.scala
+++ src/test/scala/fpinscalamulti/Vanilla/laziness/StreamTests.scala
@@ -21,6 +21,11 @@ class StreamTests extends FlatSpec with Matchers {
     s.headOption should be (None)
   }
 
+  it should "return some if the stream has a first element" in {
+    val s = Stream(1,2,3,4,5)
+    s.headOption should be (Some(1))
+  }
+
   "toList" should "force evaluation of all elements" in {
     val s = Stream(1,2,3)
     s.toList should be (List(1,2,3))
@@ -29,6 +34,83 @@ class StreamTests extends FlatSpec with Matchers {
   it should "build an empty list if the Stream is empty" in {
     val s = Stream()
     s.toList should be (Nil)
+  }
+
+  "drop" should "drop elements from a stream without evaluation" in {
+    // We force evaluation for comparison with toList
+    val s = Stream(1,2,3)
+    s.drop(1).toList should be (List(2,3))
+  }
+
+  it should "return empty if it is asked to drop more than all the elements in the stream" in {
+    val s = Stream(1,2,3)
+    s.drop(5) should be (Empty)
+  }
+
+  "take" should "return a stream with that many elements taken if it has enough" in {
+    val s = Stream(1,2,3)
+    s.take(2).toList should be (List(1,2))
+  }
+
+  it should "return empty on an empty list" in {
+    val s = Stream()
+    s.take(5) should be (Empty)
+  }
+
+  it should "return the original stream if asked to take more elements than the original stream" in {
+    val s = Stream(1,2,3)
+    s.take(5).toList should be (s.toList)
+  }
+
+  "takeWhile1" should "take while a condition is met" in {
+    val s = Stream(1,2,3,4,5)
+    s.takeWhile1(_ < 3).toList should be (List(1,2))
+  }
+
+  it should "return empty if the first does not meet the criteria" in {
+    val s = Stream(1,2,3,4,5)
+    s.takeWhile1(_ > 1) should be (Empty)
+  }
+
+  "exists" should "return true if a value in the stream matches the predicate" in {
+    val s = Stream(1,2,3,4,5)
+    s.exists(_ == 1) should be (true)
+  }
+
+  it should "return false if no values match the predicate" in {
+    val s = Stream(1,2,3,4,5)
+    s.exists(_ == 100) should be (false)
+  }
+
+  "forall" should "return true if all values of the stream match the predicate" in {
+    val s = Stream(1,2,3,4,5)
+    s.forAll(_ < 10) should be (true)
+  }
+
+  it should "return false if any value fails to match the predicate" in {
+    val s = Stream(1,2,3,4,5)
+    s.forAll(_ < 5) should be (false)
+    s.forAll(_ == 1) should be (false)
+  }
+
+  "takeWhile" should "take while a condition is met" in {
+    val s = Stream(1,2,3,4,5)
+    s.takeWhile(_ < 3).toList should be (List(1,2))
+  }
+
+  it should "return empty if the first does not meet the criteria" in {
+    val s = Stream(1,2,3,4,5)
+    s.takeWhile(_ > 1) should be (Empty)
+  }
+
+  "headOptionFoldR" should "return None on an Empty Stream" in {
+    val s = Stream()
+    s.headOptionFoldR should be (None)
+  }
+
+  it should "return some if the stream has a first element" in {
+    val s = Stream(1,2,3,4,5)
+    s.headOptionFoldR should be (Some(1))
   }
 
 }

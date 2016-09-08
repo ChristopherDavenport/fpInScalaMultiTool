bba53b973dcc0600a4b1b6a44b59c2c99bdb9233
Christopher Davenport <ChristopherDavenport@outlook.com>
2016-09-08 07:44:47 -0400

    Exercises Through 6.10 Complete

--- src/main/scala/fpinscalamulti/Vanilla/state/RNG.scala
+++ src/main/scala/fpinscalamulti/Vanilla/state/RNG.scala
@@ -65,6 +65,7 @@ object RNG {
     ((d1,d2,d3), r3)
   }
 
+  // Exercise 6.4
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
     def loop(count: Int, rng: RNG, acc: List[Int]): (List[Int], RNG) = count match {
       case positive if positive > 0 =>
@@ -86,8 +87,10 @@ object RNG {
 
   def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
 
+  // Exercise 6.5
   def _double: Rand[Double] = map(nonNegativeInt)(i => i.toDouble / Int.MaxValue.toDouble)
 
+  // Exercise 6.6
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
     val (a, r1) = ra(rng)
     val (b, r2) = rb(r1)
@@ -101,10 +104,69 @@ object RNG {
 
   def randDoubleInt: Rand[(Double, Int)] = both(double, int)
 
+  // Exercise 6.7
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
     val a = fs.foldLeft(RNG.unit(List[A]()))((acc, f) => map2(f, acc)(_ :: _))
     map(a)(_.reverse)
   }
+
+  /**
+    * Exercise 6.8
+    *
+    * This one is tricky because it is supplying extra parameters for the required function.
+    * First we deconstruct the rand application to generate the new state and the new rand, and
+    * then we must must the input to the function and the rand into as the second parameter for
+    * the type as it takes and RNG => Rand so by supplying the extra parameter we still meet those
+    * criteria
+    *
+    * @param f The original Rand
+    * @param g A Function which takes A to Rand[B]
+    * @tparam A The Type of the Original Rand
+    * @tparam B The type of the Returned Rand
+    * @return A Rand of Type B transformed by the randomness
+    */
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, r1) = f(rng)
+    g(a)(r1)
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt){ i =>
+        val mod = i % n
+        if ( i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+  }
+
+  /**
+    * Exercise 6.9 -
+    * Map Is quite simple because we take the internal value and use unit to lift the function back into the Rand
+    *
+    * @param s The Rand To Lift
+    * @param f The function to transform within the Rand
+    * @tparam A The origin Type of Rand
+    * @tparam B The final type of Raand
+    * @return A Rand of B
+    */
+  def _map[A,B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s){ b => unit(f(b))}
+
+  /**
+    * Exercise 6.9 -
+    * Map2 Uses Map and Flatmap which allows us to use a for comprehension rather than flatmap, then the call
+    * to map. Then the final function is called on the two intermediate products as though they were nested.
+    *
+    * @param ra The first rand
+    * @param rb The second Rand
+    * @param f The Function that transforms the internals of the two rands
+    * @tparam A The type of the first rand
+    * @tparam B The type of the second rand
+    * @tparam C The type of the return Rand
+    * @return A Rand of Type C
+    */
+  def _map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = for {
+    a <- ra
+    b <- rb
+  } yield f(a,b)
+
 
 
 }
--- src/main/scala/fpinscalamulti/Vanilla/state/State.scala
+++ src/main/scala/fpinscalamulti/Vanilla/state/State.scala
@@ -3,6 +3,22 @@ package fpinscalamulti.Vanilla.state
 /**
   * Created by davenpcm on 8/21/16.
   */
-class State {
+case class State[S, +A](run: S => (A,S)){
+  import State._
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+
+  def map[B](f: A => B): State[S, B] = flatMap(a => unit(f(a)))
+
+  def map2[B,C](that: State[S, B])(f: (A, B) => C): State[S, C] = this.flatMap{ a => that.map{ b => f(a, b)}}
+
+
+}
+
+object State{
+  def unit[S, A](a: A): State[S, A] = State( state => ( a, state) )
+
 
 }
